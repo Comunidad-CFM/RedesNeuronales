f915877802bd27a9d5ca291f8ef8ff5e717a89e6
hiken13 <mancraft13@hotmail.es>
2016-05-31 22:58:10 -0600

    Marcar la imagen

--- resize.py
+++ resize.py
@@ -7,3 +7,4 @@ wpercent = (basewidth / float(img.size[0]))
 hsize = int((float(img.size[1]) * float(wpercent)))
 img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
 img.save('ironmanOutPut.jpg')
+
